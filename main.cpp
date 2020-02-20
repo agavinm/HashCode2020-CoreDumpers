a3e72196961afa60584f1f28e7290333cf15e9bc
karny1998 <45491071+karny1998@users.noreply.github.com>
2020-02-20 17:49:35 +0100

    hOLA

--- main.cpp
+++ main.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 #include "readfile.h"
+using namespace std;
 
 int main() {
     readFile("CMakeLists.txt");
+    cout << "SISQUES CALVO DE MIERDA" << endl;
     return 0;
 
 }
